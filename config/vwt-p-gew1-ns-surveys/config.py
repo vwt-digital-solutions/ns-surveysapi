0a8208246a6c28c396a9ac83913a72baf2dd9dcf
dvanrhenen <d.vanrhenen@vwt.digital>
2019-05-29 12:35:24 +0200

    Activated authorization. DAT-107

--- config/vwt-p-gew1-ns-surveys/config.py
+++ config/vwt-p-gew1-ns-surveys/config.py
@@ -1,1 +1,5 @@
 BUCKET = 'vwt-p-gew1-ns-surveys-registrations-stg'
+
+OAUTH_EXPECTED_AUDIENCE = 'https://vwt-p-gew1-ns-surveys-api'
+OAUTH_EXPECTED_ISSUER = 'https://sts.windows.net/be36ab0a-ee39-47de-9356-a8a501a9c832/'
+OAUTH_JWKS_URL = 'https://login.microsoftonline.com/common/discovery/v2.0/keys'
