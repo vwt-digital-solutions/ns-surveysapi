961843c6bad83ab91898a9388cea9bcfd541b799
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-06-18 12:23:34 +0200

    prepare e2e tweak configuration. DAT-153

--- config/vwt-d-gew1-ns-surveys/config.py
+++ config/vwt-d-gew1-ns-surveys/config.py
@@ -3,3 +3,9 @@ BUCKET = 'vwt-d-gew1-ns-surveys-registrations-stg'
 OAUTH_EXPECTED_AUDIENCE = 'https://vwt-d-gew1-ns-surveys-api'
 OAUTH_EXPECTED_ISSUER = 'https://sts.windows.net/be36ab0a-ee39-47de-9356-a8a501a9c832/'
 OAUTH_JWKS_URL = 'https://login.microsoftonline.com/common/discovery/v2.0/keys'
+
+# Uncomment to activate OAUTH2 client credentials flow based on appid claim
+OAUTH_E2E_EXPECTED_AUDIENCE = 'https://vwt-d-gew1-ns-surveys-api'
+OAUTH_E2E_EXPECTED_ISSUER = 'https://login.microsoftonline.com/be36ab0a-ee39-47de-9356-a8a501a9c832/v2.0'
+OAUTH_E2E_JWKS_URL = 'https://login.microsoftonline.com/common/discovery/v2.0/keys'
+OAUTH_E2E_APPID = 'xxxx'
