f35630d38a048331d935aaa164f968e7f4601762
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-06-24 10:01:33 +0200

    Provide correct values for e2e access token validation. DAT-158

--- config/vwt-d-gew1-ns-surveys/config.py
+++ config/vwt-d-gew1-ns-surveys/config.py
@@ -5,7 +5,7 @@ OAUTH_EXPECTED_ISSUER = 'https://sts.windows.net/be36ab0a-ee39-47de-9356-a8a501a
 OAUTH_JWKS_URL = 'https://login.microsoftonline.com/common/discovery/v2.0/keys'
 
 # Uncomment to activate OAUTH2 client credentials flow based on appid claim
-OAUTH_E2E_EXPECTED_AUDIENCE = 'https://vwt-d-gew1-ns-surveys-api'
-OAUTH_E2E_EXPECTED_ISSUER = 'https://login.microsoftonline.com/be36ab0a-ee39-47de-9356-a8a501a9c832/v2.0'
+OAUTH_E2E_EXPECTED_AUDIENCE = 'https://vwt-d-gew1-ns-surveys-e2e'
+OAUTH_E2E_EXPECTED_ISSUER = 'https://sts.windows.net/be36ab0a-ee39-47de-9356-a8a501a9c832/'
 OAUTH_E2E_JWKS_URL = 'https://login.microsoftonline.com/common/discovery/v2.0/keys'
-OAUTH_E2E_APPID = 'xxxx'
+OAUTH_E2E_APPID = '7c8c8b4e-7ea4-40f7-964f-60147ece444d'
